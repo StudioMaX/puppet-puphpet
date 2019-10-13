a5e35a03b8f7b9685eefbabc5a3c601ac1b3492a
Pavel Starosek <starosekpd@gmail.com>
2019-10-13 22:54:35 +0600

    Fixed blackfire

--- manifests/blackfire/install.pp
+++ manifests/blackfire/install.pp
@@ -8,8 +8,14 @@ class puphpet::blackfire::install
 
   $blackfire = $puphpet::params::hiera['blackfire']
 
+  $settings = deep_merge($blackfire['settings'], {
+    'php' => {
+      'ini_path' => "/etc/php/${puphpet::php::params::version_match}/mods-available/blackfire.ini",
+    },
+  })
+
   create_resources('class', {
-    'blackfire' => $blackfire['settings']
+    'blackfire' => $settings
   })
 
 }
